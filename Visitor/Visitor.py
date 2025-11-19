c9f2802e6dda4486fd780e781e5bfe24155a4560
Samuel Leyton Muñoz <145390285+samuelleyton2006@users.noreply.github.com>
2025-11-19 16:38:25 +0000

    importsVisitor

--- Visitor/Visitor.py
+++ Visitor/Visitor.py
@@ -1,7 +1,13 @@
 from antlr4 import *
 from Librerias.LibreriaAritmetica import *
 from Librerias.LibreriaMatrices import *
-from Librerias.LibreriaArchivos import *
+from Librerias.LibreriaArchivosGestion import *
 from Librerias.LibreriaGraficas import *
 from Librerias.LibreriaFunciones import *
+from Librerias.LibreriaFuncion import *
+from Librerias.LibreriaGraficasTerminal import *
+from Librerias.LibreriaTablas import *
+from Librerias.LibreriaRegresionLineal import * 
+
+
 
